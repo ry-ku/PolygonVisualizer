00216374849379a1b49848d8d5848c10b371d5cb
Ryan Kuruppu <Ryan.Kuruppu@sweetsystems.se>
2025-10-29 18:26:42 +0530

    Update dist setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PolygonVisualizer/',
-})
+  base: "/PolygonVisualizer/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
